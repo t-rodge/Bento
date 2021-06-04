87245d9e88f044512814e5b44960528462935ecb
t-rodge <85374752+t-rodge@users.noreply.github.com>
2021-06-04 18:40:50 -0500

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,19 +3,19 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Thomas';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
+const gree1 = 'Go to Sleep  ';
+const gree2 = 'Good morning,  ';
+const gree3 = 'Good afternoon,  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
 const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+if (hour >= 2 && hour < 5) {
+  document.getElementById('greetings').innerText = gree1 + name + "!";
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
